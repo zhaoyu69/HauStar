6a42cba202da16f7638f6f858a5a6402f4de6e55
zhaoyu69 <zhaoyu_m69@163.com>
2019-01-11 10:39:45 +0800

    vue init.

--- .roadhogrc.mock.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const mock = {};
-
-fs.readdirSync(path.join(__dirname + '/mock')).forEach(function(file) {
-  Object.assign(mock, require('./mock/' + file))
-});
-
-export default mock;
