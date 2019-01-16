d702538d9f66f3d6fa63642746647a7643815ceb
zhaoyu69 <zhaoyu_m69@163.com>
2019-01-16 17:34:11 +0800

    write page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,66 @@
 <template>
   <div id="app">
-    <router-view/>
-    <Nav />
+    <!-- main content -->
+    <view-box ref="viewBox" body-padding-top="46px" body-padding-bottom="55px">
+      <x-header
+        slot="header"
+        :title="title"
+        style="width:100%;position:absolute;left:0;top:0;z-index:100;">
+        <x-icon slot="overwrite-left" type="navicon" size="35" style="fill:#fff;position:relative;top:-8px;left:-3px;"></x-icon>
+      </x-header>
+      <router-view/>
+      <tabbar class="tab">
+        <tabbar-item :selected="this.$route.path === '/'" link="/">
+          <img slot="icon" src="@/assets/nav_dk.png">
+          <img slot="icon-active" src="@/assets/nav_dk_active.png">
+          <span slot="label">打卡</span>
+        </tabbar-item>
+        <tabbar-item :selected="this.$route.path === '/statistics'" link="/statistics">
+          <img slot="icon" src="@/assets/nav_tj.png">
+          <img slot="icon-active" src="@/assets/nav_tj_active.png">
+          <span slot="label">统计</span>
+        </tabbar-item>
+        <tabbar-item :selected="this.$route.path === '/rewards'" link="/rewards">
+          <img slot="icon" src="@/assets/nav_jc.png">
+          <img slot="icon-active" src="@/assets/nav_jc_active.png">
+          <span slot="label">奖惩</span>
+        </tabbar-item>
+      </tabbar>
+    </view-box>
   </div>
 </template>
 
 <script>
-import Nav from '@/components/Nav'
-export default {
-  name: 'App',
-  components: {
-    Nav
+  import { Tabbar, TabbarItem, ViewBox, XHeader } from 'vux'
+  export default {
+    name: 'App',
+    components: {
+      Tabbar,
+      TabbarItem,
+      ViewBox,
+      XHeader
+    },
+    computed: {
+      title () {
+        if (this.$route.path === '/') return '打卡';
+        if (this.$route.path === '/statistics') return '统计';
+        if (this.$route.path === '/rewards') return '奖惩';
+      }
+    }
   }
-}
 </script>
 
 <style lang="less">
-@import "~vux/src/styles/reset.less";
-@import '~vux/src/styles/1px.less';
-#app {
-  height: 100vh;
-}
+  @import "~vux/src/styles/reset.less";
+  @import '~vux/src/styles/1px.less';
+  @import 'styles/font/font.css';
+  html, body{
+    height: 100%;
+    width: 100%;
+    overflow-x: hidden;
+    font-family: "pop";
+  }
+  #app {
+    height: 100%;
+  }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,110 @@
 <template>
   <div class="home">
-    Home
+    <flexbox :gutter="0">
+      <flexbox-item v-for="type in types" :key="type.id" :span="1/3" class="item">
+        <div class="item-img-wrap" v-on:click="dk(type)" v-bind:class="{active:type.isActive}">
+          <img :src="type.src" alt="">
+        </div>
+        <p class="item-label">{{ type.label }}</p>
+        <x-dialog v-model="show" class="dialog-demo" hide-on-blur>
+          <div class="log">
+            <p class="log-title">打卡日志</p>
+            <div class="log-content">
+              <p class="log-time">{{ nowTime }}</p>
+              <div style="margin-top: 10px">
+                <img :src="type.src" alt="" style="width: 32px;height: 32px;">
+                <p style="font-size: 12px;">{{ type.label }}</p>
+              </div>
+              <x-textarea :max="200" :placeholder="'写点什么吧...'" class="log-text"></x-textarea>
+            </div>
+          </div>
+        </x-dialog>
+      </flexbox-item>
+    </flexbox>
   </div>
 </template>
 
 <script>
+  import {Flexbox, FlexboxItem, XTextarea, XDialog} from 'vux';
+  import demoSrc from "@/assets/sun.png";
   export default {
-    name: 'Home'
+    components: {
+      Flexbox,
+      FlexboxItem,
+      XTextarea,
+      XDialog
+    },
+    methods: {
+      dk: function(type) {
+        type.isActive = !type.isActive;
+        this.show = type.isActive;
+      }
+    },
+    data() {
+      return {
+        nowTime: this.$moment().format('YYYY/MM/DD HH:mm:ss'),
+        show: false,
+        types: [{
+          src: demoSrc,
+          label: "1",
+          isActive: true,
+        }, {
+          src: demoSrc,
+          label: "2",
+          isActive: false,
+        }, {
+          src: demoSrc,
+          label: "3",
+          isActive: false,
+        }],
+      }
+    }
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="less">
+  .home {
+    padding: 20px 0;
+  }
+  .item {
+    text-align: center;
+    .item-img-wrap {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin: 0 auto;
+      line-height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid #f1f1f1;
+      &.active {
+        background: #fff493;
+      }
+      img {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    .item-label {
+      margin-top: 3px;
+    }
+    .log {
+      .log-title {
+        background: #09BB07;
+        color: #fff;
+        padding: 3px 0;
+      }
+      .log-content {
+        .log-time {
+          font-size: 14px;
+          color: #bbb;
+          border-bottom: 1px solid #eee;
+        }
+        .log-text {
+          font-size: 12px;
+        }
+      }
+    }
+  }
 </style>
